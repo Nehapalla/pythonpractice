9e2b9e7f5335a554228a3ac93b1640dfbf576f1b
Gayathri <g3.botta@gmail.com>
2021-06-21 19:31:54 +0100

    task 1

--- IdentifyNumber.py
+++ IdentifyNumber.py
@@ -1,1 +1,10 @@
 # Program to input from user and validate
+
+while True:
+    num = int(input("Enter number: "))
+    if -9999 < num < 9999:
+        print("valid")
+        break
+    else:
+        print("Enter a number between -9999 and 9999")
+
