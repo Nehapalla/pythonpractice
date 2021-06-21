ca2db25030dca5ad985d9dd09acd877d6c3fcfb1
Nehapalla <84698019+Nehapalla@users.noreply.github.com>
2021-06-21 21:19:57 +0100

    Create Identifynumber_n.py

---
+++ Identifynumber_n.py
@@ -0,0 +1,4 @@
+Num = int(input("Enter any number. "))
+while  Num <= -9999 or Num >= 9999:
+  Num = int(input("Enter a new number between -9999 and 9999. "))
+print("You have chosen a valid number. ")
