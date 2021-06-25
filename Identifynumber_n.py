714dc8190071355b1651702155b4be4293956ace
Nehapalla <84698019+Nehapalla@users.noreply.github.com>
2021-06-25 22:06:09 +0100

    Update Identifynumber_n.py

--- Identifynumber_n.py
+++ Identifynumber_n.py
@@ -1,4 +1,10 @@
-Num = int(input("Enter any number. "))
-while  Num <= -9999 or Num >= 9999:
-  Num = int(input("Enter a new number between -9999 and 9999. "))
-print("You have chosen a valid number. ")
+while True:
+  try:
+    Num = float(input("Enter any number. "))
+  except ValueError:
+    print("You have chosen an invalid number. Enter a new number between -9999 and 9999.")
+    continue
+  while Num < -9999 or Num > 9999:
+    Num = float(input("Enter a new number between -9999 and 9999. "))
+  print("You have chosen a valid number. ")
+  
